fb489cdb472fed85d94c46f6e157464d420af22c
simpsric <simpsric@gmail.com>
2025-04-06 10:25:14 -0400

    Add initial implementation of Window class and main application entry point
    - Created main.py to serve as the application entry point.
    - Introduced constructor_classes module with __init__.py to facilitate imports.
    - Developed Window class in window_class.py for GUI window management using Tkinter.

---
+++ constructor_classes/window_class.py
@@ -0,0 +1,24 @@
+from tkinter import Tk, BOTH, Canvas
+
+class Window:
+    def __init__(self, width, height):
+        self.__root = Tk()
+        self.__root.title("Maze Solver 9000")
+        self.__root.geometry(f"{width}x{height}")
+        self.canvas = Canvas(self.__root, width=width, height=height)
+        self.canvas.pack(fill=BOTH, expand=1)
+        self.running_window = False
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
+        
+    def redraw(self):
+        self.__root.update_idletasks()
+        self.__root.update()
+        
+    def wait_for_close(self):
+        self.running_window = True
+        while self.running_window:
+            self.redraw()
+        self.__root.destroy()
+        
+    def close(self):
+        self.running_window = False
---
+++ main.py
@@ -0,0 +1,10 @@
+from constructor_classes.window_class import Window
+
+def main():
+    window = Window(800, 600)
+    window.wait_for_close()
+    
+
+
+if __name__ == "__main__":
+    main()
